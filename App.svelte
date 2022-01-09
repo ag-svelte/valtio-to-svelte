4c582fb7dd9e6ab70ce0aae198c2c1e9ba3e78fb
Anthoni Gardner <AnthoniG@users.noreply.github.com>
2022-01-09 02:40:45 +0000

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import data, { toSvelteStore } from "./data";
+
+  const snapshotStore = toSvelteStore(data);
+  const countSnapshotStore = toSvelteStore(data.count);
+
+  $: console.log("snapshotStore changed!", $snapshotStore);
+  $: console.log("countSnapshotStore changed!", $countSnapshotStore);
+</script>
+
+<button on:click={() => data.count.value--}>-</button>
+{$snapshotStore.count.value}
+<button on:click={() => data.count.value++}>+</button>
+
+<input bind:value={data.text} />
+{$snapshotStore.text}
